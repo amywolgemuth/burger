59aab0d7f26b94dd70ad31d3eb2be6d6fae5ba19
Amy Wolf <awolgemuth@trinisys.loc>
2019-08-06 13:35:42 -0500

    added all folders and files that I'll be using with routing

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('quarter pounder with cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('California Burger', true);
+
